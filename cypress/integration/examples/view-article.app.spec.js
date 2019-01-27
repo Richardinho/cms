aa0aacda48ee6619086a68252dd0fd6f85edaa30
Richard Hunter <richard@richardhunter.co.uk>
2019-01-27 17:31:56 +0000

    cypress test for clicking through to view article page

--- cypress/integration/examples/view-article.app.spec.js
+++ cypress/integration/examples/view-article.app.spec.js
@@ -12,12 +12,26 @@ describe('view and article', () => {
           ]
         }
       });
+
+      cy.route({
+        url: 'http://october.richardhunter.co.uk/index.php/api/article/*',
+        response: {
+          body: '# Hello world\n\nits the end of the world\n\nas we **know** it.'
+        }
+      });
     });
 
     it('should display article summaries', () => {
       cy.visit('/');
       cy.get('.link span').eq(0).should('contain', 'Hello world');
       cy.get('.link span').eq(1).should('contain', 'Goodbye');
+    });
+
+    describe('when the user clicks on the \'view article\` button', () => {
+      it('should navigate to article', () => {
+        cy.get('.link .button').eq(0).click();
+        cy.contains('its the end of the world');
+      });
     });
   });
 });
