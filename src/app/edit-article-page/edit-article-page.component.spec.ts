1977d47b564b7e0468a89eaab07f2b381e8b1de7
Richard Hunter <richard@richardhunter.co.uk>
2019-02-16 14:49:06 +0000

    Unit test for error handling in edit article page

--- src/app/edit-article-page/edit-article-page.component.spec.ts
+++ src/app/edit-article-page/edit-article-page.component.spec.ts
@@ -3,9 +3,9 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { Article } from '../article';
 import { ArticleService } from '../article.service';
 import { EditArticlePageComponent } from './edit-article-page.component';
-import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { ActivatedRouteStub } from '../testing/activated-route-stub';
-import { of } from 'rxjs';
+import { defer, of } from 'rxjs';
 
 
 @Pipe({ name: 'markdownToHTML' })
@@ -15,42 +15,126 @@ class MockMarkdownToHTMLPipe {
   }
 }
 
-xdescribe('EditArticlePageComponent', () => {
+export function asyncError<T>(errorObject: any) {
+  return defer(() => Promise.reject(errorObject));
+}
+
+describe('EditArticlePageComponent', () => {
   let component: EditArticlePageComponent;
   let fixture: ComponentFixture<EditArticlePageComponent>;
 
-  const stubArticle = { id: 3, title: 'this is foo' };
   let articleServiceSpy;
+  let routerSpy;
 
-  beforeEach(async(() => {
-    articleServiceSpy = jasmine.createSpyObj('ArticleService', ['getArticle']);
-    articleServiceSpy.getArticle.and.returnValue(of(stubArticle as Article));
+  describe('when the user is not logged in', () => {
+  
+    beforeEach(async(() => {
+      //  Given
+      articleServiceSpy = jasmine.createSpyObj('ArticleService', ['getArticle']);
+      const errorResponse = {
+        status: 401
+      };
+      articleServiceSpy.getArticle.and.returnValue(asyncError(errorResponse));
+    
+      routerSpy = jasmine.createSpyObj('Router', ['navigate']);
 
-    TestBed.configureTestingModule({
-      declarations: [
-        EditArticlePageComponent, 
-        MockMarkdownToHTMLPipe,
-      ],
-      providers: [
-        { provide: ActivatedRoute, useValue: new ActivatedRouteStub({ id: 5}) },
-        { provide: ArticleService, useValue: articleServiceSpy },
-      ],
-    })
-    .compileComponents();
-  }));
+      TestBed.configureTestingModule({
+        declarations: [
+          EditArticlePageComponent,
+          MockMarkdownToHTMLPipe,
+        ],
+        providers: [
+          { provide: ActivatedRoute, useValue: new ActivatedRouteStub({ id: 5}) },
+          { provide: Router, useValue: routerSpy },
+          { provide: ArticleService, useValue: articleServiceSpy },
+        ]
+      }).compileComponents();
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(EditArticlePageComponent);
-    component = fixture.componentInstance;
-    const activatedRoute = TestBed.get(ActivatedRoute);
-    fixture.detectChanges();
+      //  When 
+      fixture = TestBed.createComponent(EditArticlePageComponent);
 
+      fixture.detectChanges();
+    }));
+  
+    it('should redirect user to login page', () => {
+      //  Then
+      expect(routerSpy.navigate).toHaveBeenCalledWith(['/login']);
+    });
   });
 
-  it('should fetch article from service', () => {
-    expect(articleServiceSpy.getArticle).toHaveBeenCalledWith(5);
-    expect(component.article).toEqual({ id: 3, title: 'this is foo'});
-    expect(fixture.nativeElement.querySelector('.article-content')
-      .textContent).toBe('hello world');
+  describe('when any other kind of error occurs', () => {
+    it('should display an error message', async(() => {
+      //  Given
+      articleServiceSpy = jasmine.createSpyObj('ArticleService', ['getArticle']);
+      const errorResponse = {};
+      articleServiceSpy.getArticle.and.returnValue(asyncError(errorResponse)); 
+    
+      routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+
+      TestBed.configureTestingModule({
+        declarations: [
+          EditArticlePageComponent,
+          MockMarkdownToHTMLPipe,
+        ],
+        providers: [
+          { provide: ActivatedRoute, useValue: new ActivatedRouteStub({ id: 5 })},
+          { provide: Router, useValue: routerSpy },
+          { provide: ArticleService, useValue: articleServiceSpy },
+        ]
+      }).compileComponents();
+
+      //  When
+      fixture = TestBed.createComponent(EditArticlePageComponent);
+
+      fixture.detectChanges();
+
+      fixture.whenStable().then(() => {
+        fixture.detectChanges();
+
+        //  Then
+        expect(fixture.nativeElement.querySelector('.error-message')
+          .textContent).toBe('an error occurred');
+      });
+    }));
+  });
+
+  describe('when an valid article exists', () => {
+    beforeEach(async(() => {
+      //  Given
+      const stubArticle = { id: 3, title: 'this is foo' };
+      articleServiceSpy = jasmine.createSpyObj('ArticleService', ['getArticle']);
+      articleServiceSpy.getArticle.and.returnValue(of(stubArticle as Article));
+
+      routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+
+      TestBed.configureTestingModule({
+        declarations: [
+          EditArticlePageComponent, 
+          MockMarkdownToHTMLPipe,
+        ],
+        providers: [
+          { provide: ActivatedRoute, useValue: new ActivatedRouteStub({ id: 5}) },
+          { provide: Router, useValue: routerSpy },
+          { provide: ArticleService, useValue: articleServiceSpy },
+        ],
+      })
+        .compileComponents();
+    }));
+
+    beforeEach(() => {
+      //  When 
+      fixture = TestBed.createComponent(EditArticlePageComponent);
+      component = fixture.componentInstance;
+
+      fixture.detectChanges();
+    });
+
+    it('should fetch article from service', () => {
+      //  Then
+      expect(articleServiceSpy.getArticle).toHaveBeenCalledWith(5);
+      expect(component.article).toEqual({ id: 3, title: 'this is foo'});
+      expect(fixture.nativeElement.querySelector('.article-content')
+        .textContent).toBe('hello world');
+    });
   });
 });
