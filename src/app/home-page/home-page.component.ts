ee897c9e226464e920b3c578f2f4d713664f0639
Richard Hunter <richard@richardhunter.co.uk>
2019-02-16 22:41:22 +0000

    Adding back in some changes from home component that had disappeared

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Article } from '../article';
 import { ArticleService } from '../article.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
@@ -9,14 +10,27 @@ import { ArticleService } from '../article.service';
 })
 export class HomePageComponent implements OnInit {
   articles: Array<Article>;
+  errorMessage: string;
 
   constructor(
-    private articleService: ArticleService) {}
+    private articleService: ArticleService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.articleService.getArticles()
       .subscribe((data: Array<Article>) => {
         this.articles = data;
+      }, (e) => {
+        /*
+         * If user is forbidden to access this page, then we redirect to login page.
+         * Otherwise, we just show a generic error message
+         */
+        if (e.status && e.status === 401) {
+          this.router.navigate(['/login']);
+        } else {
+          this.errorMessage = 'Some error occurred';
+        }
       });
   }
 
