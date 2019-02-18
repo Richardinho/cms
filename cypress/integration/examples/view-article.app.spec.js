d5e98f734faf5752304867291e84fcd67900bb7d
Richard Hunter <richard@richardhunter.co.uk>
2019-02-18 20:21:26 +0000

    Cypress test for login happy path

--- cypress/integration/examples/view-article.app.spec.js
+++ cypress/integration/examples/view-article.app.spec.js
@@ -1,37 +1,85 @@
-describe('view and article', () => {
-  describe('when the user navigates to the home page', () => {
+describe('valid username and password', () => {
+  it('should allow users to sign in on login page', () => {
+    GIVEN_user_is_not_signed_in();
+    AND_visits_login_page();
+    WHEN_user_inputs_username_and_password();
+    AND_clicks_submit();
+    THEN_they_should_be_redirected_to_home_page();
+  });
 
-    beforeEach(() => {
-      cy.server();
-      cy.route({
-        url: 'http://october.richardhunter.co.uk/index.php/api/articles/',
-        response: {
-          articles: [
-            { id: 1, title: 'Hello world' },
-            { id: 2, title: 'Goodbye' }
-          ]
-        }
-      });
+  it('should redirect to login page if user who is not signed in navigates to a page', () => {
+    GIVEN_user_is_not_signed_in();
+    WHEN_user_visits_article_page();
+    THEN_they_should_be_redirected_to_login_page();
+    WHEN_user_inputs_username_and_password();
+    AND_clicks_submit();
+    THEN_they_should_be_redirected_to_article_page();
+  });
 
-      cy.route({
-        url: 'http://october.richardhunter.co.uk/index.php/api/article/*',
-        response: {
-          body: '# Hello world\n\nits the end of the world\n\nas we **know** it.'
-        }
-      });
+  beforeEach(() => {
+    cy.server();
+
+    cy.route({
+      url: '**/index.php/api/login',
+      method: 'POST',
+      response: {
+        jwt_token: 'mock-token',
+      },
     });
 
-    it('should display article summaries', () => {
-      cy.visit('/');
-      cy.get('.link span').eq(0).should('contain', 'Hello world');
-      cy.get('.link span').eq(1).should('contain', 'Goodbye');
+    cy.route({
+      url: '**/index.php/api/articles/',
+      response: {
+        articles: [
+          { id: 1, title: 'Hello world' },
+          { id: 2, title: 'Goodbye' }
+        ]
+      }
     });
 
-    describe('when the user clicks on the \'view article\` button', () => {
-      it('should navigate to article', () => {
-        cy.get('.link .button').eq(0).click();
-        cy.contains('its the end of the world');
-      });
+    cy.route({
+      url: '**/index.php/api/article/*',
+      response: {
+        body: '# Hello world\n\nits the end of the world\n\nas we **know** it.'
+      }
     });
   });
 });
+
+function AND_visits_login_page() {
+  cy.visit('/login');
+}
+
+function WHEN_user_visits_home_page() {
+  cy.visit('/home');
+}
+
+function WHEN_user_visits_article_page() {
+  cy.visit('/edit-article/11');
+}
+
+function THEN_they_should_be_redirected_to_article_page() {
+  cy.get('.bar').should('contain', 'bar');
+}
+
+function THEN_they_should_be_redirected_to_login_page() {
+  cy.get('.username');
+}
+
+function WHEN_user_inputs_username_and_password() {
+  cy.get('.username').type('some_username');
+  cy.get('.password').type('some_password');
+}
+
+function AND_clicks_submit() {
+  cy.get('#login-button').click();
+}
+
+function GIVEN_user_is_not_signed_in() {
+  //  probably not necessary
+  window.localStorage.removeItem('jwt_token');
+}
+
+function THEN_they_should_be_redirected_to_home_page() {
+  cy.get('#foo').should('contain', 'This is foo');
+}
