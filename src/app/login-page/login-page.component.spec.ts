b62a984b608891b74e2e1c30aa270018cb57f316
Richard Hunter <richard@richardhunter.co.uk>
2019-06-23 15:14:32 +0100

    refactoring login test

--- src/app/login-page/login-page.component.spec.ts
+++ src/app/login-page/login-page.component.spec.ts
@@ -8,86 +8,89 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 describe('LoginPageComponent', () => {
-  let authServiceSpy;
-  let routerSpy;
-  let httpSpy;
+  let authServiceStub;
+  let routerStub;
+  let httpStub;
   let fixture: ComponentFixture<LoginPageComponent>;
+  let component: LoginPageComponent;
 
   beforeEach(() => {
-    httpSpy = jasmine.createSpyObj('HttpClient', ['post']);
-    routerSpy = jasmine.createSpyObj('Router', ['navigate']);
-    authServiceSpy = jasmine.createSpyObj('AuthService', ['setToken', 'logOut']);
+    httpStub = jasmine.createSpyObj('HttpClient', ['post']);
+    routerStub = jasmine.createSpyObj('Router', ['navigate']);
+    authServiceStub = jasmine.createSpyObj('AuthService', ['setToken', 'logOut']);
   });
-
   
   describe('when the user submits a correct username and password', () => {
     beforeEach(() => {
-      httpSpy.post.and.returnValue(of({
+      httpStub.post.and.returnValue(of({
         jwt_token: 'blah',
       }));
     });
 
     describe('and there is NOT a stored redirect url', () => {
-      it('should set the jwt token and redirect to url', () => {
-        //  Given
-        const component = new LoginPageComponent(httpSpy, routerSpy, authServiceSpy);
+      beforeEach(() => {
+        component = new LoginPageComponent(httpStub, routerStub, authServiceStub);
 
-        //  When
         component.onSubmit();
+      });
 
-        //  Then
-        expect(authServiceSpy.setToken).toHaveBeenCalledWith('blah');
-        expect(routerSpy.navigate).toHaveBeenCalledWith(['/home']);
+      it('should set the jwt token', () => {
+        expect(authServiceStub.setToken).toHaveBeenCalledWith('blah');
+      });
+
+      it('it should redirect to url', () => {
+        expect(routerStub.navigate).toHaveBeenCalledWith(['/home']);
       });
     });
 
     describe('and there is a stored redirect url', () => {
-      it('should set the jwt token and redirect to home page', () => {
-        //  Given
-        authServiceSpy.redirectUrl = '/foobar';
-        const component = new LoginPageComponent(httpSpy, routerSpy, authServiceSpy);
+      beforeEach(() => {
+        authServiceStub.redirectUrl = '/foobar';
+        const component = new LoginPageComponent(httpStub, routerStub, authServiceStub);
 
-        //  When
         component.onSubmit();
+      });
 
-        //  Then
-        expect(authServiceSpy.setToken).toHaveBeenCalledWith('blah');
-        expect(routerSpy.navigate).toHaveBeenCalledWith(['/foobar']);
+      it('should set the jwt token', () => {
+        expect(authServiceStub.setToken).toHaveBeenCalledWith('blah');
+      });
+
+      it('should redirect to home page', () => {
+        expect(routerStub.navigate).toHaveBeenCalledWith(['/foobar']);
       });
     });
   });
 
   describe('when the user submits an incorrect username and password', () => {
     it('should show error telling the user that they submitted the wrong username/password pair', () => {
-      //  Given
       const errorResponse = {
         status: 403,
       };
 
-      httpSpy.post.and.returnValue(throwError(errorResponse));
+      httpStub = jasmine.createSpyObj('HttpClient', {
+        post: throwError(errorResponse)
+      });
 
-      const component = new LoginPageComponent(httpSpy, routerSpy, authServiceSpy);
+      const component = new LoginPageComponent(httpStub, routerStub, authServiceStub);
 
-      //  When
       component.onSubmit();
 
-      //  Then
       expect(component.errorMessage).toBe('Your submitted username and password were wrong');
      });
   });
 
   describe('when some other error occurs', () => {
     it('should show generic error', async(() => {
-      //  Given
       const errorResponse = {};
-      httpSpy.post.and.returnValue(throwError(errorResponse));
 
-      const component = new LoginPageComponent(httpSpy, routerSpy, authServiceSpy);
+      httpStub = jasmine.createSpyObj('HttpClient', {
+        post: throwError(errorResponse)
+      });
 
-      //  When
+      const component = new LoginPageComponent(httpStub, routerStub, authServiceStub);
+
       component.onSubmit();
 
-      //  Then
       expect(component.errorMessage).toBe('Some other error occurred');
     }));
   });
