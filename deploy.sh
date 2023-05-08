ffb02f4df0752a67fa7ec497cbab8a1a2a06a3a4
Richard Hunter <richard@richardhunter.co.uk>
2023-05-09 00:51:27 +0100

    copying over dist folder to server

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,6 @@
 echo "this is the deploy script"
+
+# delete existing folder
+ssh richardh@richardhunter.co.uk 'rm -rf public_html/cms'
+
+scp -r dist/cms richardh@richardhunter.co.uk:public_html/cms
